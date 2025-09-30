81e4d37d551946b03e3747d76cff3f4e2ab6cf4c
dopaemon <polarisdp@gmail.com>
2025-09-30 17:23:21 +0700

    Artus: Add Infomation Ram
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- internal/libutils/ram.go
+++ internal/libutils/ram.go
@@ -1,5 +1,39 @@
 package libutils
 
 import (
+	"fmt"
+
 	"github.com/shirou/gopsutil/v4/mem"
 )
+
+func GetTotalRAM() (string, error) {
+	vm, err := mem.VirtualMemory()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%.2f GB", float64(vm.Total)/(1024*1024*1024)), nil
+}
+
+func GetUsedRAM() (string, error) {
+	vm, err := mem.VirtualMemory()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%.2f GB", float64(vm.Used)/(1024*1024*1024)), nil
+}
+
+func GetFreeRAM() (string, error) {
+	vm, err := mem.VirtualMemory()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%.2f GB", float64(vm.Available)/(1024*1024*1024)), nil
+}
+
+func GetRAMUsagePercent() (string, error) {
+	vm, err := mem.VirtualMemory()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%.2f%%", vm.UsedPercent), nil
+}
