b4f899248a67d2c383001434e52bbbdb886383f6
dopaemon <polarisdp@gmail.com>
2025-09-30 19:06:59 +0700

    Artus: Call Infomation Net
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -78,4 +78,33 @@ func main() {
 	} else {
 		fmt.Println("Err:", err)
 	}
+
+	iface, err := libutils.GetPrimaryInterface()
+	if err != nil {
+		fmt.Println("Err:", err)
+	}
+
+	if sent, err := libutils.GetNetBytesSent(iface); err == nil {
+		fmt.Println("Bytes sent:", sent)
+	} else {
+		fmt.Println("Err:", err)
+	}
+
+	if recv, err := libutils.GetNetBytesRecv(iface); err == nil {
+		fmt.Println("Bytes received:", recv)
+	} else {
+		fmt.Println("Err:", err)
+	}
+
+	if psent, err := libutils.GetNetPacketsSent(iface); err == nil {
+		fmt.Println("Packets sent:", psent)
+	} else {
+		fmt.Println("Err:", err)
+	}
+
+	if precv, err := libutils.GetNetPacketsRecv(iface); err == nil {
+		fmt.Println("Packets received:", precv)
+	} else {
+		fmt.Println("Err:", err)
+	}
 }
