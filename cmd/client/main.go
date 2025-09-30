34e8eec9f9870cf1427b03154af3a18c6a7f3298
dopaemon <polarisdp@gmail.com>
2025-09-30 17:15:05 +0700

    Artus: vendor: Initial Vendor
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,8 +8,26 @@ import (
 
 func main() {
 	if name, err := libutils.GetCPUName(); err == nil {
-		fmt.Println("CPU Name: ", name)
+		fmt.Println("CPU Name:", name)
 	} else {
-		fmt.Println("Err: ", err)
+		fmt.Println("Err:", err)
+	}
+
+	if cores, err := libutils.GetLogicalCores(); err == nil {
+		fmt.Println("CPU Count Logical Core:", cores)
+	} else {
+		fmt.Println("Err:", err)
+	}
+
+	if cores, err := libutils.GetPhysicalCores(); err == nil {
+		fmt.Println("CPU Count Physial Core:", cores)
+	} else {
+		fmt.Println("Err:", err)
+	}
+
+	if usage, err := libutils.GetCPUUsage(); err == nil {
+		fmt.Println("CPU Usage:", usage)
+	} else {
+		fmt.Println("Err:", err)
 	}
 }
